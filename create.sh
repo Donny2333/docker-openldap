c1db487f598fac056965e8e1104bd89cad5aa476
Donny <1021001390@qq.com>
2023-05-04 09:02:19 +0800

    first commit

---
+++ create.sh
@@ -0,0 +1,29 @@
+docker rm -f openldap
+docker run \
+    -d \
+    -p 389:389 \
+    -p 636:636 \
+    -v $(pwd)/ldap:/usr/local/ldap \
+    -v $(pwd)/data/openldap/ldap:/var/lib/ldap \
+    -v $(pwd)/data/openldap/slapd.d:/etc/ldap/slapd.d \
+    --env LDAP_ORGANISATION="Manager" \
+    --env LDAP_DOMAIN="example.com" \
+    --env LDAP_ADMIN_PASSWORD="123456" \
+    --name openldap \
+    --hostname openldap-host\
+    --net my-network --ip 172.20.1.10 \
+    osixia/openldap:stable-amd64
+
+# Login DN: cn=admin,dc=example,dc=com
+# Password: 123456
+
+docker rm -f phpldapadmin
+docker run \
+    -p 10001:80 \
+    --privileged \
+    --name phpldapadmin \
+    --net my-network --ip 172.20.1.11 \
+    --link openldap:openldap \
+    --env PHPLDAPADMIN_HTTPS=false \
+    --env PHPLDAPADMIN_LDAP_HOSTS=openldap  \
+    -d osixia/phpldapadmin:stable-amd64
